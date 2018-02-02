272796f40990f33409b81a34965c312acf588862
Keegan <keegan@kairuni.com>
2018-02-01 19:16:01 -0800

    DDatabase changes, mostly. Updated classpath for sql shenanigans, not sure it will play nice on Windows.

--- src/mhw_asb/Application.java
+++ src/mhw_asb/Application.java
@@ -1,10 +1,44 @@
 package mhw_asb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Foo");
+		// Load sqlite JDBC driver
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			
+			
+			conn = DriverManager.getConnection("jdbc:sqlite:database.db");
+			
+			Statement stmt = conn.createStatement();
+			stmt.setQueryTimeout(30);
+			
+			ResultSet res = stmt.executeQuery("SELECT * FROM Equipment WHERE Name like \"Zorah%\"");
+			while(res.next()) {
+				System.out.println(res.getInt("ID"));
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {         
+	        try {
+	              if(conn != null)
+	                 conn.close();
+	        } catch(SQLException e) {  // Use SQLException class instead.          
+	        	System.err.println(e); 
+	        }
+		}
 	}
-
 }
